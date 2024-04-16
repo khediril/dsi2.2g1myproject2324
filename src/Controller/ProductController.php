83b3d87513b72a0ca54dfd91f0d8d4adb4b9541c
khediril <khediril@yahoo.fr>
2024-04-16 17:14:36 +0100

    les CRUD

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,11 +12,33 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/product', name: 'app_product_')]
 class ProductController extends AbstractController
 {
-    #[Route('/list', name: 'list')]
+    #[Route('/', name: 'list')]
     public function index(ProductRepository $productRepository): Response
     {
         $products = $productRepository->findAll();
         
         return $this->render('product/list.html.twig', ['products' => $products]);
+    }
+   
+    #[Route('/{id}', name: 'detail')]
+    public function detail($id,ProductRepository $productRepository): Response
+    {
+        $product = $productRepository->find($id);
+        
+        return $this->render('product/detail.html.twig', ['product' => $product]);
+    }
+    #[Route('/delete/{id}', name: 'delete')]
+    public function delete($id,ProductRepository $productRepository,EntityManagerInterface $em): Response
+    {
+        $product = $productRepository->find($id);
+        $em->remove($product);
+        $em->flush();
+        $this->addFlash(
+            'notice',
+            'Produit supprime avec succes!'
+        );
+        
+        //return $this->render('product/delete.html.twig', []);
+        return $this->redirectToRoute('app_product_list');
     }
 }
